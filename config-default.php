7755761c80776257e197fc272d446cbfdf054e8c
jcfrog <jerome.choain@gmail.com>
2022-05-16 17:11:14 +0200

    reorganizing

--- config-default.php
+++ config-default.php
@@ -1,20 +1,5 @@
 <?php
-
-/* Site identity */
-$siteTitle = "Titre trombinoscope";
-$siteSubtitle = "Sous-titre pour mon trombinoscope";
-
-/* according to your place */
-setlocale(LC_ALL, 'fr_FR.UTF-8');
-
-
-/* edit mode password : put the md5 of your password, you can create it here https://www.md5.fr/ */ 
-$md5pw = "3bfdb73af5d1ab6f5d947439ca96352f";
-
 /* Database */
-define('DB_PATH', './database/contacts.db'); // SQLite data base file path
-define('TABLE_NAME',"contacts"); // name of the table in the data base
-
 // fields handled in the data base
 $fields = array(
     // "name" and "firstname" are needed, for the rest it's up to you
@@ -28,10 +13,22 @@ $fields = array(
     "comments" => array("input" => "textarea", "label" => "Informations diverses")
 );
 
+define('DB_PATH', './database/contacts.db'); // SQLite data base file path
+define('TABLE_NAME',"contacts"); // name of the table in the data base
+
+/* edit mode password : put the md5 of your password, you can create it here https://www.md5.fr/ */ 
+$md5pw = "3bfdb73af5d1ab6f5d947439ca96352f";
+
+/* Site identity */
+$siteTitle = "Titre trombinoscope";
+$siteSubtitle = "Sous-titre pour mon trombinoscope";
+
+/* according to your place */
+setlocale(LC_ALL, 'fr_FR.UTF-8');
+
 /* Pagination : number of displayed contacts per page */ 
 define("NB_PER_PAGE", 8);
 
 /* User default anonymous avatar */
 define('DEFAULT_PIC','avatars/user.png');
-
 ?>
